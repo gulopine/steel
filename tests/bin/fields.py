eb15f61a4ddbc7f5ae5665c8b803324c0a4312fc
Marty Alchin <marty@martyalchin.com>
2011-01-04 07:38:27 -0500

    Tests for some signed number representations

--- tests/bin/fields.py
+++ tests/bin/fields.py
@@ -19,5 +19,21 @@ class EndiannessTest(unittest.TestCase):
         self.assertEqual(field.decode(encoded_value), self.decoded_value)
 
 
+class SigningTest(unittest.TestCase):
+    decoded_value = -42
+
+    def test_SignMagnitude(self):
+        signer = fields.SignMagnitude()
+        encoded_value = 0b10101010
+        self.assertEqual(bin(signer.encode(self.decoded_value, size=1)), bin(encoded_value))
+        self.assertEqual(signer.decode(encoded_value, size=1), self.decoded_value)
+
+    def test_OnesComplement(self):
+        signer = fields.OnesComplement()
+        encoded_value = 0b11010101
+        self.assertEqual(bin(signer.encode(self.decoded_value, size=1)), bin(encoded_value))
+        self.assertEqual(signer.decode(encoded_value, size=1), self.decoded_value)
+
+
 if __name__ == '__main__':
     unittest.main()
