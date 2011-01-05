75330d4ab6aa048f69947a7bfbd2a8cde64f5f23
Marty Alchin <marty@martyalchin.com>
2011-01-04 19:04:36 -0500

    Don't test more than you need to

--- tests/bin/fields.py
+++ tests/bin/fields.py
@@ -7,16 +7,16 @@ class EndiannessTest(unittest.TestCase):
     decoded_value = 42
     
     def test_BigEndian(self):
-        field = fields.Integer(size=2, endianness=fields.BigEndian())
+        endianness = fields.BigEndian()
         encoded_value = b'\x00*'
-        self.assertEqual(field.encode(self.decoded_value), encoded_value)
-        self.assertEqual(field.decode(encoded_value), self.decoded_value)
+        self.assertEqual(endianness.encode(self.decoded_value, size=2), encoded_value)
+        self.assertEqual(endianness.decode(encoded_value, size=2), self.decoded_value)
 
     def test_LittleEndian(self):
-        field = fields.Integer(size=2, endianness=fields.LittleEndian())
+        endianness = fields.LittleEndian()
         encoded_value = b'*\x00'
-        self.assertEqual(field.encode(self.decoded_value), encoded_value)
-        self.assertEqual(field.decode(encoded_value), self.decoded_value)
+        self.assertEqual(endianness.encode(self.decoded_value, size=2), encoded_value)
+        self.assertEqual(endianness.decode(encoded_value, size=2), self.decoded_value)
 
 
 class SigningTest(unittest.TestCase):
