52c32472a50cd7a87951ec5d5ed94e63268d5ca3
Marty Alchin <marty@martyalchin.com>
2011-02-06 23:18:49 -0500

    Closer to JPG support (but not yet)

--- examples/images/jpg.py
+++ examples/images/jpg.py
@@ -1,3 +1,4 @@
+import sys
 from biwako import bin
 
 DENSITY_UNITS = (
@@ -7,63 +8,86 @@ DENSITY_UNITS = (
 )
 
 
+class EmptyChunk(bin.Chunk):
+    id = bin.Integer(size=2)
+    size = bin.Bytes(size=0)
+    payload = bin.Payload(size=0)
+
+
 class Chunk(bin.Chunk):
-    ff = bin.FixedNumber(0xFF)
-    type = bin.Integer(size=1)
+    id = bin.Integer(size=2)
     size = bin.Integer(size=2)
-    payload = bin.Bytes(size=size - 2)
+    payload = bin.Payload(size=size - 2)
 
 
-Chunk(0xD8)
+class ScanChunk(bin.Chunk):
+    id = bin.Integer(size=2)
+    size = bin.Integer(size=2)
+    payload = bin.Payload(size=size - 2)
+
+
+@EmptyChunk(0xFFD8)
 class Start(bin.Structure):
-    size = None
-    payload = None
+    pass
 
 
-Chunk(0xE0)
+@Chunk(0xFFE0)
 class Header(bin.Structure):
-    marker = bin.FixedString('JFIF\x00')
-    major_version = bin.PositiveInteger(size=1)
-    minor_version = bin.PositiveInteger(size=1)
-    density_units = bin.PositiveInteger(size=1, choicse=DENSITY_UNITS)
-    x_density = bin.PositiveInteger(size=2)
-    y_density = bin.PositiveInteger(size=2)
-    thumb_width = bin.PositiveInteger(size=1)
-    thumb_height = bin.PositiveInteger(size=1)
-    thumb_data = bin.ByteString(size=3 * thumb_width * thumb_height)
-    
+    marker = bin.FixedString(b'JFIF\x00')
+    major_version = bin.Integer(size=1)
+    minor_version = bin.Integer(size=1)
+    density_units = bin.Integer(size=1, choicse=DENSITY_UNITS)
+    x_density = bin.Integer(size=2)
+    y_density = bin.Integer(size=2)
+    thumb_width = bin.Integer(size=1)
+    thumb_height = bin.Integer(size=1)
+    thumb_data = bin.Bytes(size=3 * thumb_width * thumb_height)
+
     @property
     def version(self):
-        return u'%d.%02d' % (self.major_version, self.minor_version)
+        return '%i.%02i' % (self.major_version, self.minor_version)
 
 
-Chunk(0xC0)
+@Chunk(0xFFC0)
 class StartFrame(bin.Structure):
-    precision = bin.PositiveInteger(size=1)
-    width = bin.PositiveInteger(size=2)
-    height = bin.PositiveInteger(size=2)
+    precision = bin.Integer(size=1)
+    width = bin.Integer(size=2)
+    height = bin.Integer(size=2)
 
+# 0xFD96 is at offset 0xA8A
 
-Chunk(0xFE)
+@Chunk(0xFFFE)
 class Comment(bin.Structure):
-    value = bin.String(encoding='utf8', size=bin.REMAINDER)
+    value = bin.String(encoding='utf8', size=bin.Remainder)
+
+    def __str__(self):
+        return self.value
 
 
-Chunk(0xD9)
+@EmptyChunk(0xFFD9)
 class End(bin.Structure):
-    size = None
-    payload = None
+    pass
 
 
-class JFIF(bin.Structure, endianness=bin.BigEndian):
-    header = Header()
-    chunks = bin.ChunkList(Chunk)
+class JFIF(bin.Structure):
+    start = bin.SubStructure(Start)
+    header = bin.SubStructure(Header)
+    chunks = bin.ChunkList(Chunk, (StartFrame, Comment), terminator=End)
 
-    width = bin.ChunkValue(StartFrame.width)
-    height = bin.ChunkValue(StartFrame.height)
-    comment = bin.ChunkValue(Comment.value)
+    @property
+    def width(self):
+        return self.chunks.of_type(StartFrame)[0].width
+
+    @property
+    def height(self):
+        return self.chunks.of_type(StartFrame)[0].height
+
+    @property
+    def comment(self):
+        return self.chunks.of_type(Comment)[0].value
 
 
 if __name__ == '__main__':
-    jpg = JPG(open(sys.argv[1], 'rb'))
-    print '%s x %s' % (jpg.width, jpg.height)
+    jpg = JFIF(open(sys.argv[1], 'rb'))
+    print(jpg.chunks)
+    print('%s x %s' % (jpg.width, jpg.height))
