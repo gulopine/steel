95f20c5a44c19ddea76dfdd74663820fb1ccd15e
Marty Alchin <marty@martyalchin.com>
2011-03-05 23:04:24 -0500

    Closer to bit structures, but not there yet

--- biwako/bin/bits.py
+++ biwako/bin/bits.py
@@ -1,9 +1,8 @@
-from biwako import bin
-from ..common import DeclarativeMetaclass
+from biwako import bin, common
 from .fields import args
 
 
-class BitStructureMetaclass(bin.DeclarativeMetaclass):
+class BitStructureMetaclass(common.DeclarativeMetaclass):
     @classmethod
     def __prepare__(cls, name, bases, **options):
         return super(BitStructureMetaclass, cls).__prepare__(name, bases, **options)
@@ -39,7 +38,8 @@ class Structure(bin.Structure, metaclass=BitStructureMetaclass):
             # Read more bytes from the file
             read_size = int(((size - self._bits_left) + 7) / 8)
             field = bin.Integer(size=read_size)
-            bytes, value = field.read_value(self)
+            bytes = self._file.read(read_size)
+            value = field.decode(bytes)
             bit_buffer = (bit_buffer << size) | value
         self.bits_left = 8 % size
         bits = bit_buffer >> self.bits_left
@@ -47,15 +47,129 @@ class Structure(bin.Structure, metaclass=BitStructureMetaclass):
         return bits
 
 
-class Integer(bin.Integer):
-    def __init__(self, *args, **kwargs):
-        super(Flag, self).__init__(*args, size=int((size + 7) / 8), **kwargs)
-        self.size = size
+class Field(metaclass=common.DeclarativeFieldMetaclass):
+    size = args.Argument(resolve_field=True)
+    choices = args.Argument(default=())
+    default = args.Argument(default=args.NotProvided)
+
+    def __init__(self, label='', **kwargs):
+        self.label = label
+
+        for name, arg in self.arguments.items():
+            if name in kwargs:
+                value = kwargs.pop(name)
+            elif arg.has_default:
+                value = arg.default
+            else:
+                raise TypeError("The %s argument is required for %s fields" % (arg.name, self.__class__.__name__))
+            setattr(self, name, value)
+        if kwargs:
+            raise TypeError("%s is not a valid argument for %s fields" % (list(kwargs.keys())[0], self.__class__.__name__))
+
+        # Once the base values are all in place, arguments can be initialized properly
+        for name, arg in self.arguments.items():
+            setattr(self, name, arg.initialize(self, getattr(self, name)))
+
+    def for_instance(self, instance):
+        return self
+
+    def read(self, obj):
+        # If the size can be determined easily, read
+        # that number of bytes and return it directly.
+        if self.size is not None:
+            return obj.read(self.size)
+
+        # Otherwise, the field needs to supply its own
+        # technique for determining how much data to read.
+        raise NotImplementedError()
+
+    def write(self, obj, value):
+        # By default, this doesn't do much, but individual
+        # fields can/should override it if necessary
+        obj.write(value)
+
+    def set_name(self, name):
+        self.name = name
+        label = self.label or name.replace('_', ' ')
+        self.label = label.title()
+
+    def attach_to_class(self, cls):
+        cls._fields.append(self)
+
+    def validate(self, obj, value):
+        # This should raise a ValueError if the value is invalid
+        # It should simply return without an error if it's valid
+
+        # First, make sure the value can be encoded
+        self.encode(value)
+
+        # Then make sure it's a valid option, if applicable
+        if self.choices and value not in set(v for v, desc in self.choices):
+            raise ValueError("%r is not a valid choice" % value)
+
+    def _extract(self, instance):
+        try:
+            return self.read(instance), None
+        except FullyDecoded as obj:
+            return obj.bytes, obj.value
+
+    def read_value(self, file):
+        try:
+            bytes = self.read(file)
+            value = self.decode(bytes)
+            return bytes, value
+        except FullyDecoded as obj:
+            return obj.bytes, obj.value
+
+    def __get__(self, instance, owner):
+        if not instance:
+            return self
+
+        try:
+            value = instance._extract(self)
+        except IOError:
+            if self.default is not args.NotProvided:
+                return self.default
+            raise AttributeError("Attribute %r has no data" % field.name)
+
+        if self.name not in instance.__dict__:
+            instance.__dict__[self.name] = self.decode(value)
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+        instance._raw_values[self.name] = self.encode(value)
+
+    def __repr__(self):
+        return '<%s: %s>' % (self.name, type(self).__name__)
+
+
+class Integer(Field):
+    size = args.Argument()
+    signed = args.Argument(default=False)
 
     def encode(self, value):
+        print(value)
         if value > (1 << self.size) - 1:
             raise ValueError("Value is large for this field.")
-        return super(Integer, self).encode(value) & ((1 << self.size) - 1)
+        return value & ((1 << self.size) - 1)
+
+
+class FixedInteger(Integer):
+    def __init__(self, value, *args, **kwargs):
+        super(FixedInteger, self).__init__(*args, signed=value < 0, **kwargs)
+        self.decoded_value = value
+        self.encoded_value = super(FixedInteger, self).encode(value)
+
+    def encode(self, value):
+        if value != self.decoded_value:
+            raise ValueError('Expected %r, got %r.' % (self.decoded_value, value))
+        return self.encoded_value
+
+    def decode(self, value):
+        if value != self.encoded_value:
+            raise ValueError('Expected %r, got %r.' % (self.encoded_value, value))
+        return self.decoded_value
 
 
 class Flag(Integer):
