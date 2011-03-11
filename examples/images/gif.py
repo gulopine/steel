2685c1346c447567eeb5b2768ab8e6838b9590f0
Marty Alchin <marty@martyalchin.com>
2011-03-10 23:14:15 -0500

    Organize a little better, more closely matching the actual spec

--- examples/images/gif.py
+++ examples/images/gif.py
@@ -7,11 +7,27 @@ VERSIONS = (
     ('89a', '89a'),
 )
 
+
+class ScreenDescriptor(bin.Structure, endianness=bin.LittleEndian):
+    width = bin.Integer(size=2)
+    height = bin.Integer(size=2)
+    info_byte = bin.Integer(size=1)  # TODO: Decode this
+    background_color = bin.Integer(size=1)
+    Reserved(size=1)
+
+
 class GIF(bin.Structure, endianness=bin.LittleEndian, encoding='ascii'):
     tag = bin.FixedString('GIF')
     version = bin.String(size=3, choices=VERSIONS)
-    width = bin.Integer(size=2)
-    height = bin.Integer(size=2)
+    screen = bin.SubStructure(ScreenDescriptor)
+
+    @property
+    def width(self):
+        return self.screen.width
+
+    @property
+    def height(self):
+        return self.screen.height
 
 
 if __name__ == '__main__':
