5b180be99b0a2a0245b25f4ba8f3b101a91b4193
Marty Alchin <marty@martyalchin.com>
2011-03-11 13:56:00 -0500

    Some untested changesto bit structures

--- biwako/bin/bits.py
+++ biwako/bin/bits.py
@@ -1,5 +1,6 @@
-from biwako import bin, common
-from .fields import args
+import sys
+
+from biwako import bin, common, args
 
 
 class BitStructureMetaclass(common.DeclarativeMetaclass):
@@ -45,6 +46,9 @@ class Structure(bin.Structure, metaclass=BitStructureMetaclass):
         bits = bit_buffer >> self.bits_left
         self._bit_buffer = bit_buffer & (1 << self._bits_left) - 1
         return bits
+
+    def get_raw_bytes(self):
+        return b''
 
 
 class Field(metaclass=common.DeclarativeFieldMetaclass):
@@ -149,7 +153,13 @@ class Integer(Field):
     signed = args.Argument(default=False)
 
     def encode(self, value):
-        print(value)
+        print('Encoding %s' % value)
+        if value > (1 << self.size) - 1:
+            raise ValueError("Value is large for this field.")
+        return value & ((1 << self.size) - 1)
+
+    def decode(self, value):
+        print('Decoding %s' % value)
         if value > (1 << self.size) - 1:
             raise ValueError("Value is large for this field.")
         return value & ((1 << self.size) - 1)
@@ -180,5 +190,37 @@ class Flag(Integer):
 
     def decode(self, value):
         return bool(super(Flag, self).decode(value))
+
+
+class Reserved(Field):
+    default = args.Override(default=None)
+
+    def __init__(self, *args, **kwargs):
+        super(Reserved, self).__init__(*args, **kwargs)
+
+        # Hack to add the reserved field to the class without
+        # having to explicitly give it a (likely useless) name
+        frame = sys._getframe(2)
+        locals = frame.f_locals
+        locals[self.get_available_name(locals.keys())] = self
+
+    def get_available_name(self, locals):
+        i = 0
+        while True:
+            name = '_reserved_%s' % i
+            if name not in locals:
+                return name
+            i += 1
+
+    def set_name(self, name):
+        if hasattr(self, 'name'):
+            raise TypeError('Reserved fields must not be given an attribute name')
+        super(Reserved, self).set_name(name)
+
+    def encode(self, value):
+        return 2 ** self.size - 1
+
+    def decode(self, value):
+        return None
 
 
