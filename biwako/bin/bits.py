a78607e060f86d82342411e39d8d441ffbb80ffa
Marty Alchin <marty@martyalchin.com>
2011-03-11 23:13:21 -0500

    Got bit structures working! Needs tests, though

--- biwako/bin/bits.py
+++ biwako/bin/bits.py
@@ -1,4 +1,5 @@
 import sys
+import builtins
 
 from biwako import bin, common, args
 
@@ -42,13 +43,28 @@ class Structure(bin.Structure, metaclass=BitStructureMetaclass):
             bytes = self._file.read(read_size)
             value = field.decode(bytes)
             bit_buffer = (bit_buffer << size) | value
-        self.bits_left = 8 % size
-        bits = bit_buffer >> self.bits_left
+            self._bits_left += read_size * 8
+        self._bits_left -= size % 8
+        bits = bit_buffer >> self._bits_left
         self._bit_buffer = bit_buffer & (1 << self._bits_left) - 1
         return bits
 
     def get_raw_bytes(self):
-        return b''
+        output = b''
+        bits_read = 0
+        byte = 0
+        for field in self.__class__._fields:
+            value = getattr(self, field.name)
+            if field.name not in self._raw_values:
+                setattr(self, field.name, getattr(self, field.name))
+            bits_read += field.size
+            bits = self._raw_values[field.name]
+            byte = (byte << field.size) + bits
+            if bits_read > 8:
+                byte >>= bits_read - (bits_read % 8)
+                bits_read -= bits_read - (bits_read % 8)
+            output += bytes([byte])
+        return output
 
 
 class Field(metaclass=common.DeclarativeFieldMetaclass):
@@ -153,13 +169,11 @@ class Integer(Field):
     signed = args.Argument(default=False)
 
     def encode(self, value):
-        print('Encoding %s' % value)
         if value > (1 << self.size) - 1:
             raise ValueError("Value is large for this field.")
         return value & ((1 << self.size) - 1)
 
     def decode(self, value):
-        print('Decoding %s' % value)
         if value > (1 << self.size) - 1:
             raise ValueError("Value is large for this field.")
         return value & ((1 << self.size) - 1)
