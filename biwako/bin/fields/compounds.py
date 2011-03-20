47eb306bcf1f55a60bce3daeb129b9972265dda2
Marty Alchin <marty@martyalchin.com>
2011-03-19 22:53:06 -0400

    Incomplete support for referencing parent objects form sub-structures

--- biwako/bin/fields/compounds.py
+++ biwako/bin/fields/compounds.py
@@ -15,6 +15,11 @@ class SubStructure(Field):
     def read(self, file):
         value = self.structure(file)
 
+        if type(value) in Parent.assigned_names:
+            name = Parent.assigned_names[type(value)]
+            print('Assigning parent (%r) to value (%r) as %s!' % (file, value, name))
+            setattr(value, name, file)
+
         # Force the evaluation of the entire structure in
         # order to make sure other fields work properly
         value_bytes = value.get_raw_bytes()
@@ -63,5 +68,46 @@ class List(Field):
         for value in values:
             encoded_values.append(instance_field.encode(value))
         return b''.join(encoded_values)
+
+
+class Parent(Field):
+    assigned_names = {}
+    size = args.Override(default=None)
+
+    def __init__(self, parent=None, attr=None):
+        self._parent = parent
+        self.attr = attr
+        super(Parent, self).__init__()
+
+    def __getattr__(self, name):
+        # Don't interfere with private attributes or Python's magic methods
+        if not name.startswith('_'):
+            print('Getting attribute %r' % name)
+            return Parent(self, name)
+        raise AttributeError(name)
+
+    def resolve(self, instance):
+        print('Resolving parent')
+        print('Resolving parent: %s' % self.name)
+        print('For instance %r' % instance)
+        if self._parent is not None:
+            instance = self._parent.resolve(instance)
+        return getattr(instance, self.name)
+
+    def set_name(self, name):
+        print('Setting name to %r' % name)
+        self.name = name
+
+    def attach_to_class(self, cls):
+        self.assigned_names[cls] = self.name
+
+    def read(self, size=None):
+        return b''
+
+    def encode(self, value):
+        return value
+
+    def decode(self, value):
+        return value
 
 
