be5f1ea487b5008b7ac9a0066af01d6b8557f05f
Marty Alchin <marty@martyalchin.com>
2013-10-05 17:06:02 -0700

    IFF chunks are always big-endian

--- steel/chunks/iff.py
+++ steel/chunks/iff.py
@@ -1,6 +1,7 @@
 import collections
 import io
 
+from steel.fields.numbers import BigEndian
 from steel import fields
 from steel.chunks import base
 
@@ -9,13 +10,13 @@ __all__ = ['Chunk', 'Form']
 
 class Chunk(base.Chunk):
     id = fields.String(size=4, encoding='ascii')
-    size = fields.Integer(size=4)
+    size = fields.Integer(size=4, endianness=BigEndian)
     payload = base.Payload(size=size)
 
 
 class Form(base.Chunk, encoding='ascii'):
     tag = fields.FixedString('FORM')
-    size = fields.Integer(size=4)
+    size = fields.Integer(size=4, endianness=BigEndian)
     id = fields.String(size=4)
     payload = base.Payload(size=size)
 
