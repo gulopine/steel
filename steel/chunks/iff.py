c9b6387702baee3da0a3bca8b302b619e69893f7
Marty Alchin <marty@martyalchin.com>
2013-10-05 17:06:22 -0700

    Customize a ChunkList just for IFF chunks

--- steel/chunks/iff.py
+++ steel/chunks/iff.py
@@ -5,13 +5,19 @@ from steel.fields.numbers import BigEndian
 from steel import fields
 from steel.chunks import base
 
-__all__ = ['Chunk', 'Form']
+__all__ = ['Chunk', 'ChunkList', 'Form']
 
 
 class Chunk(base.Chunk):
     id = fields.String(size=4, encoding='ascii')
     size = fields.Integer(size=4, endianness=BigEndian)
     payload = base.Payload(size=size)
+
+
+class ChunkList(base.ChunkList):
+    def __init__(self, *args, **kwargs):
+        # Just a simple override to default to a list of IFF chunks
+        return super(ChunkList, self).__init__(Chunk, *args, **kwargs)
 
 
 class Form(base.Chunk, encoding='ascii'):
